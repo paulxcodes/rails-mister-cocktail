a57a52f931674881dd0b52de653b261072738a04
Paul Veliscovici <paul.veliscovici@gmail.com>
2021-02-20 13:38:07 -0500

    seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'cleaning ingredients'
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
+
+ingredients['drinks'].each do |ingredient|
+  new_ingredient = Ingredient.create(
+    name: ingredient['strIngredient1']
+  )
+  puts "Ingredient #{new_ingredient.id} is created"
+end
+
